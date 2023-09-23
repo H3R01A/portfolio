1c306bdf9160b92ff6bf9025e192a35c238e3e7b
H3R01A <aallen0001@gmail.com>
2023-09-23 14:55:30 -0700

    react router working

--- client/App.jsx
+++ client/App.jsx
@@ -1,11 +1,31 @@
 import React from 'react';
+import AboutPage from './components/AboutPage.jsx';
+import WelcomePage from "./components/WelcomePage.jsx";
+import Project1 from './components/Project1.jsx'
+import {
+   Routes,
+   Route,
+   Link
+  } from "react-router-dom";
 
-import WelcomePage from "./components/WelcomePage.jsx"
+
+
+
+
 
 export default function App(){
     return (
         <>
-        <WelcomePage/>
+        <Routes>
+        <Route path="/" element={<WelcomePage/>}></Route>
+        <Route path="/about" element={<AboutPage/>}/>
+        <Route path='/project1' element={<Project1/>}/>
+
+        </Routes>
+        <br></br>
+        <Link to="/">Home</Link>
+        <br></br>
+        <Link to="/about">About</Link>
         </>
     )
 }
--- client/components/WelcomePage.jsx
+++ client/components/WelcomePage.jsx
@@ -1,4 +1,10 @@
 import React from 'react';
+import {
+    Routes,
+    Route,
+    Link
+   } from "react-router-dom";
+ 
 
 
 export default function WelcomePage(){
@@ -6,6 +12,7 @@ export default function WelcomePage(){
     return(
         <>
         <h1>Hello!</h1>
+        <Link to="/project1"><button>New Game</button></Link>
         </>
     )
 }
