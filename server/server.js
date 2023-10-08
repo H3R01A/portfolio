c04739e5f0575181c3d3d8b3280d62b9fc5d82da
H3R01A <aallen0001@gmail.com>
2023-10-07 17:01:55 -0700

    added functionality to utilize environment variables. Also updated server.js file to utilize enviornment variables

--- server/server.js
+++ server/server.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const app = express();
+const dotenv = require('dotenv')
+dotenv.config();
 const path = require('path');
-const port = 3000;
 const cors = require('cors')
 
+
+let port = process.env.PORT;
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(cors())
+
+if(port === null || port === ""){
+    port = 3000;
+}
 
 // app.get('/', (req, res) => {
 //     return res.status(200).json('made it to the backend')
