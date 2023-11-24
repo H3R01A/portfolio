5864008e8582cd2f56807d7150f443f8278f1233
H3R01A <aallen0001@gmail.com>
2023-11-23 16:28:36 -0800

    update data.js file for API calls and show correct data in Project component

--- client/data.js
+++ client/data.js
@@ -1,9 +1,8 @@
-
 const data = new Map();
 
-data.set(1, {'description': 'ToDo List', 'tech_stack': ['React', 'Postgres', 'MongoDB']});
-data.set(2, {'description': 'Subslify', 'tech_stack': ['React', 'MUI', 'Docker']});
-data.set(3, {'description': 'Codename Hermes', 'tech_stack': ['Kafka', 'RabbitMQ']});
+data.set(1, {'owner': 'h3r01a','repo': 'codesmith-solo-project', 'projectName': 'Daily Quest','tech_stack': ['React', 'Postgres', 'MongoDB', 'Docker']});
+data.set(2, {'owner':'Scratch-Tech-Software', 'repo': 'Subslify', 'projectName': 'Subslify','tech_stack': ['React', 'MUI', 'Docker']});
+data.set(3, {'owner': 'oslabs-beta','repo': 'Codename-Hermes', 'projectName': 'Codename Hermes','tech_stack': ['Kafka', 'RabbitMQ']});
 
 
 export default data;
