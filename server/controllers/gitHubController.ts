6ad02e4aa66ac3e4b41913baa27d0fbdc4b3f78f
H3R01A <aallen0001@gmail.com>
2023-11-24 22:11:21 -0800

    update gitHubController.ts for additional middleware function to get languages

--- server/controllers/gitHubController.ts
+++ server/controllers/gitHubController.ts
@@ -4,20 +4,18 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+const octokit = new Octokit({
+  auth: process.env.GH_TKN
+});
+
 const gitHubController = {
 
     getRepo: async (req: Request, res: Response, next: NextFunction) => {
 
         const {owner, repo} = req.body;
         
-        const octokit = new Octokit({
-            auth: process.env.GH_TKN
-          });
-    
           try{
 
-             //!Original repo GET Request
-    
              const result = await octokit.request('GET /repos/{owner}/{repo}', {
               owner,
               repo,
@@ -26,12 +24,58 @@ const gitHubController = {
               }
             })
 
-             res.locals.gitHubData = result.data;
+             res.locals.gitHubRepoData = result.data;
 
             console.log(result.data);
 
+            next();
 
-          // console.log(`this is the result from GitHub ${result.data.full_name}`);
+          }
+          catch(e){
+            if(e instanceof Error){
+                console.log(`An error occured with attempting to fetch from GitHub ${e.message}`);
+                next(e);
+            }
+          }
+      
+    },
+
+    getRepoLanguages: async (req: Request, res: Response, next: NextFunction) => {
+
+      const {owner, repo} = req.body;
+
+      try{
+
+         const result = await octokit.request('GET /repos/{owner}/{repo}/languages', {
+          owner,
+          repo,
+          headers: {
+            'X-GitHub-Api-Version': '2022-11-28'
+          }
+        })
+
+        res.locals.gitHubRepoLanguages = result.data;
+        next();
+
+
+      }
+
+      catch(e){
+        if(e instanceof Error){
+            console.log(`An error occured with attempting to fetch from GitHub ${e.message}`)
+            next(e);
+        }
+      }
+
+      
+
+    }
+
+}
+
+export default gitHubController;
+
+  // console.log(`this is the result from GitHub ${result.data.full_name}`);
 
           // res.locals.gitHubData = result.data;
 
@@ -46,14 +90,7 @@ const gitHubController = {
 
             // console.log(`this is the result from GitHub ${result2.data}`);
 
-            //!Org Get Languages Works! Just need to change owner and repo
-            // const result2 = await octokit.request('GET /repos/{owner}/{repo}/languages', {
-            //   owner: 'oslabs-beta', //Scratch-Tech-Software
-            //   repo: 'Codename-Hermes',
-            //   headers: {
-            //     'X-GitHub-Api-Version': '2022-11-28'
-            //   }
-            // })
+           
 
             //  const result2 = await octokit.request('GET /repos/{owner}/{repo}', {
             //     owner: 'oslabs-beta',
@@ -62,23 +99,3 @@ const gitHubController = {
             //       'X-GitHub-Api-Version': '2022-11-28'
             //     }
             //   })
-
-
-           
-
-           
-
-            next();
-
-          }
-          catch(e){
-            if(e instanceof Error){
-                console.log(`An error occured with attempting to fetch from GitHub ${e.message}`)
-            }
-          }
-      
-    }
-
-}
-
-export default gitHubController;
