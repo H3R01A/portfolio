68eb2fbb3a67454295960a6cd5595400869d06fa
H3R01A <aallen0001@gmail.com>
2023-11-26 10:30:00 -0800

    update gitHubController.ts for formatting and to remove unnecessary comments

--- server/controllers/gitHubController.ts
+++ server/controllers/gitHubController.ts
@@ -1,101 +1,65 @@
-import {Request, Response, NextFunction} from 'express';
-import { Octokit } from "octokit";
+import { Request, Response, NextFunction } from 'express';
+import { Octokit } from 'octokit';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
 const octokit = new Octokit({
-  auth: process.env.GH_TKN
+  auth: process.env.GH_TKN,
 });
 
 const gitHubController = {
+  getRepo: async (req: Request, res: Response, next: NextFunction) => {
+    const { owner, repo } = req.body;
 
-    getRepo: async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const result = await octokit.request('GET /repos/{owner}/{repo}', {
+        owner,
+        repo,
+        headers: {
+          'X-GitHub-Api-Version': '2022-11-28',
+        },
+      });
 
-        const {owner, repo} = req.body;
-        
-          try{
+      res.locals.gitHubRepoData = result.data;
 
-             const result = await octokit.request('GET /repos/{owner}/{repo}', {
-              owner,
-              repo,
-              headers: {
-                'X-GitHub-Api-Version': '2022-11-28'
-              }
-            })
+      next();
+    } catch (e) {
+      if (e instanceof Error) {
+        console.log(
+          `An error occured with attempting to fetch from GitHub ${e.message}`
+        );
+        next(e);
+      }
+    }
+  },
 
-             res.locals.gitHubRepoData = result.data;
+  getRepoLanguages: async (req: Request, res: Response, next: NextFunction) => {
+    const { owner, repo } = req.body;
 
-            console.log(result.data);
-
-            next();
-
-          }
-          catch(e){
-            if(e instanceof Error){
-                console.log(`An error occured with attempting to fetch from GitHub ${e.message}`);
-                next(e);
-            }
-          }
-      
-    },
-
-    getRepoLanguages: async (req: Request, res: Response, next: NextFunction) => {
-
-      const {owner, repo} = req.body;
-
-      try{
-
-         const result = await octokit.request('GET /repos/{owner}/{repo}/languages', {
+    try {
+      const result = await octokit.request(
+        'GET /repos/{owner}/{repo}/languages',
+        {
           owner,
           repo,
           headers: {
-            'X-GitHub-Api-Version': '2022-11-28'
-          }
-        })
-
-        res.locals.gitHubRepoLanguages = result.data;
-        next();
-
-
-      }
-
-      catch(e){
-        if(e instanceof Error){
-            console.log(`An error occured with attempting to fetch from GitHub ${e.message}`)
-            next(e);
+            'X-GitHub-Api-Version': '2022-11-28',
+          },
         }
+      );
+
+      res.locals.gitHubRepoLanguages = result.data;
+      next();
+    } catch (e) {
+      if (e instanceof Error) {
+        console.log(
+          `An error occured with attempting to fetch from GitHub ${e.message}`
+        );
+        next(e);
       }
-
-      
-
     }
-
-}
+  },
+};
 
 export default gitHubController;
-
-  // console.log(`this is the result from GitHub ${result.data.full_name}`);
-
-          // res.locals.gitHubData = result.data;
-
-            //!Org Get Request
-            //!WORKS
-            // const result2 = await octokit.request('GET /orgs/{org}/repos', {
-            //     org: 'oslabs-beta', //Scratch-Tech-Software
-            //     headers: {
-            //       'X-GitHub-Api-Version': '2022-11-28'
-            //     }
-            //   })
-
-            // console.log(`this is the result from GitHub ${result2.data}`);
-
-           
-
-            //  const result2 = await octokit.request('GET /repos/{owner}/{repo}', {
-            //     owner: 'oslabs-beta',
-            //     repo: 'Codename-Hermes',
-            //     headers: {
-            //       'X-GitHub-Api-Version': '2022-11-28'
-            //     }
-            //   })
