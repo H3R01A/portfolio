f71264437531a879f6de5ce59a294edfb3a758a4
H3R01A <aallen0001@gmail.com>
2023-11-26 10:18:44 -0800

    update data.js for appropriate repo information to retreive GitHub data

--- client/data.js
+++ client/data.js
@@ -1,8 +1,8 @@
 const data = new Map();
 
-data.set(1, {'owner': 'h3r01a','repo': 'codesmith-solo-project', 'projectName': 'Daily Quest','tech_stack': ['React', 'Postgres', 'MongoDB', 'Docker']});
-data.set(2, {'owner':'Scratch-Tech-Software', 'repo': 'Subslify', 'projectName': 'Subslify','tech_stack': ['React', 'MUI', 'Docker']});
-data.set(3, {'owner': 'oslabs-beta','repo': 'Codename-Hermes', 'projectName': 'Codename Hermes','tech_stack': ['Kafka', 'RabbitMQ']});
+data.set(1, {'owner': 'h3r01a','repo': 'Daily-Quest', 'projectName': 'Daily Quest','tech_stack': ['React', 'Postgres', 'MongoDB', 'Docker']});
+data.set(2, {'owner':'Scratch-Tech-Software', 'repo': 'Subslify', 'projectName': 'Subslify','tech_stack': ['React', 'MUI']});
+data.set(3, {'owner': 'oslabs-beta','repo': 'Codename-Hermes', 'projectName': 'Codename Hermes','tech_stack': ['Kafka', 'RabbitMQ', 'Docker']});
 
 
 export default data;
