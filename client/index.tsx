e7f5437501149ce357224701cda11cbed6b1aac1
H3R01A <aallen0001@gmail.com>
2023-12-04 21:31:04 -0800

    update index.ts for correct file path for output css file

--- client/index.tsx
+++ client/index.tsx
@@ -1,7 +1,7 @@
 import App from './App';
 import {createRoot} from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import './output';
+import '../build/output';
 
 const container = document.getElementById('root') as Element;
 const root = createRoot(container!);
