7a6648e99821b1ff1313ce52f11e10508af31c26
H3R01A <aallen0001@gmail.com>
2023-12-05 22:20:10 -0800

    update App.tsx and WelcomePage.tsx for styling to match figma design

--- client/App.tsx
+++ client/App.tsx
@@ -1,30 +1,25 @@
-import {useState} from 'react';
+import { useState } from 'react';
 import AboutPage from './components/AboutPage';
-import WelcomePage from "./components/WelcomePage";
+import WelcomePage from './components/WelcomePage';
 import Project from './components/Project';
-import {
-   Routes,
-   Route,
-   Link
-  } from "react-router-dom";
+import { Routes, Route, Link } from 'react-router-dom';
 
-export default function App(){
+export default function App() {
+  const [projectNum, setProjectNum] = useState(1);
 
-    const [projectNum, setProjectNum] = useState(1);
-
-   const handleProject = (num: number, projectTotalNum: number) => {
-    
+  const handleProject = (num: number, projectTotalNum: number) => {
     //reassign projectNum with passed in num
     // setProjectNum(projectNum + num);
-    
+
     // console.log(`expected projectNum ${projectNum + num}`)
     // console.log({projectNum, projectTotalNum});
 
     //determine what project number to return
     //if the expected projectNum is greater than set projectNum to be the end of the list
-    if(projectNum + num > projectTotalNum) return setProjectNum(projectTotalNum);
+    if (projectNum + num > projectTotalNum)
+      return setProjectNum(projectTotalNum);
     //if expected projectNum is equal to 0 than set projectNum to be the beginning of the list
-    if(projectNum + num === 0) return setProjectNum(1);
+    if (projectNum + num === 0) return setProjectNum(1);
     //else set project num to be the expected projectNum
     return setProjectNum(projectNum + num);
 
@@ -34,21 +29,32 @@ export default function App(){
 
     // return num + projectNum > 0 ? setProjectNum(num + projectNum) : setProjectNum(1);
 
-
-   }
-    return (
-        <>
-        <div className="bg-xanadu-50">
+    //bg-herotheme-300 opacity-95
+  };
+  return (
+    <>
+      <div className="bg-header-pattern p-10 flex justify-evenly">
         {/* <p className='text-blue-100'>Home</p> */}
-        <Link to="/" className="font-bold m-10">Home</Link>
-        <Link to="/about" className="font-bold">About</Link>
-        </div>
-        <Routes>
-        <Route path="/" element={<WelcomePage/>}></Route>
-        <Route path="/about" element={<AboutPage/>}/>
-        <Route path='/project' element={<Project projectNum = {projectNum} handleProject = {handleProject}/>}/>
-        </Routes>
-        
-        </>
-    )
+        <Link to="/" className="font-bold">
+          Home
+        </Link>
+        <Link to="/about" className="font-bold">
+          Projects
+        </Link>
+        <Link to="/about" className="font-bold">
+          About
+        </Link>
+      </div>
+      <Routes>
+        <Route path="/" element={<WelcomePage />}></Route>
+        <Route path="/about" element={<AboutPage />} />
+        <Route
+          path="/project"
+          element={
+            <Project projectNum={projectNum} handleProject={handleProject} />
+          }
+        />
+      </Routes>
+    </>
+  );
 }
--- client/components/WelcomePage.tsx
+++ client/components/WelcomePage.tsx
@@ -5,7 +5,7 @@ export default function WelcomePage() {
     <>
       <section className="text-center">
         <Link to="/project">
-          <button className="bg-cyan-950 text-xanadu-50 rounded-lg text-center m-72 p-5 hover:bg-blue-300 active:bg-blue-700 focus:outline-none focus:ring focus:ring-blue-300 shadow-md">
+          <button className="bg-herotheme-400 text-herotheme-300 font-serounded-lg text-center m-72 p-5 hover:bg-blue-300 active:bg-blue-700 focus:outline-none focus:ring focus:ring-blue-300 shadow-md">
             New Game
           </button>
         </Link>
