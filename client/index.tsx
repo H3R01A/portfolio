a984235edf9d3565cb1465015625efa27f8bcc09
H3R01A <aallen0001@gmail.com>
2023-12-26 22:26:24 -0800

    update index.tsx to use output.css from appropriate location

--- client/index.tsx
+++ client/index.tsx
@@ -1,7 +1,7 @@
 import App from './App';
 import {createRoot} from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import '../build/output.css';
+import './output.css';
 
 const container = document.getElementById('root') as Element;
 const root = createRoot(container!);
