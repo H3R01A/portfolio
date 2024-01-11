a1d888a034c6fcdc123fb26c38073a038fd41266
H3R01A <aallen0001@gmail.com>
2024-01-10 21:09:47 -0800

    update App.tsx component to remove navigation links and move them to WelcomePage component

--- client/App.tsx
+++ client/App.tsx
@@ -8,29 +8,23 @@ export default function App() {
   const [projectNum, setProjectNum] = useState(1);
 
   const handleProject = (num: number, projectTotalNum: number) => {
-    
     if (projectNum + num === 0) return setProjectNum(1);
-    
+
     return setProjectNum(projectNum + num);
-
-
   };
   return (
     <>
-      <div className="bg-header-pattern bg-center p-10 uppercase text-heroTheme-500 flex justify-center space-x-12">
+      <div className="bg-header-pattern bg-center p-10 uppercase text-heroTheme-500">
         {/* <p className='text-blue-100'>Home</p> */}
         <Link to="/" className="font-bold">
           Home
         </Link>
-        <Link to="/project" className="font-bold">
-          Projects
-        </Link>
-        <Link to="/contact" className="font-bold">
-          Contact
-        </Link>
       </div>
       <Routes>
-        <Route path="/" element={<WelcomePage setProjectNum = {setProjectNum}/>}></Route>
+        <Route
+          path="/"
+          element={<WelcomePage setProjectNum={setProjectNum} />}
+        ></Route>
         <Route path="/contact" element={<ContactPage />} />
         <Route
           path="/project"
