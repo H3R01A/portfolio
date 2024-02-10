89dbfc1901e9464f2e1f620a08ea083aa5093a39
H3R01A <aallen0001@gmail.com>
2024-02-10 14:02:41 -0800

    add About page for more information about dev

---
+++ client/components/AboutPage.tsx
@@ -0,0 +1,33 @@
+import { Link } from 'react-router-dom';
+
+export default function AboutPage() {
+  return (
+    <div className="flex flex-wrap items-center justify-center text-white">
+      <h1 className="mt-40 flex w-full items-center justify-center text-3xl">
+        Aaron is an active engineer with expertise in building full-stack
+        applications using technologies such as JavaScript (ES6+), React,
+        Typescript, Node.js, Tailwind CSS, Postgres, and Docker. He is
+        passionate about creating engaging web experiences and follows
+        empathetic development practices that prioritize user satisfaction.
+        Aaron's commitment goes beyond meeting technical requirements; he
+        actively considers the broader impact on users and stakeholders.
+      </h1>
+      <h1 className="flex w-full items-center justify-center text-3xl">
+        His recent focus has been on Codename Hermes, an open-source project
+        where he serves as an active contributor. Codename Hermes is a library
+        designed to abstract the implementation of message brokers like Kafka
+        and RabbitMQ, aiming to streamline the developer experience.
+      </h1>
+      <h1 className="flex w-full items-center justify-center text-3xl">
+        In addition to his technical prowess, Aaron brings strong collaboration
+        as well as leadership and communication skills to his software
+        engineering endeavors, ensuring effective teamwork and project success.
+      </h1>
+      <Link to="/">
+        <button className="font-serounded-lg text-shadow rounded-md px-7 py-3.5 text-center font-buttonText text-portfolioNG uppercase text-white hover:animate-pulse hover:border-b-[3px] hover:border-l-[.3px] hover:border-r-[3px] hover:border-t-[.3px] hover:border-heroTheme-300 hover:bg-emerald-500 hover:text-black hover:shadow-[4px_4px_4.8px_0px_rgba(0,0,0,0.2)] mt-12">
+          Back to Main Menu
+        </button>
+      </Link>
+    </div>
+  );
+}
