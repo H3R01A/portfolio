a2d5282c47dd5f0534b5eb1fbf8e575f4a7e00b3
H3R01A <aallen0001@gmail.com>
2024-02-10 14:03:18 -0800

    update styling for better readability

--- client/components/ContactPage.tsx
+++ client/components/ContactPage.tsx
@@ -2,25 +2,25 @@ import { Link } from 'react-router-dom';
 
 export default function ContactPage() {
   return (
-    <div className="flex items-center justify-center flex-wrap">
-      <h1 className="w-full uppercase text-3xl flex items-center justify-center mt-40">
+    <div className="flex flex-wrap items-center justify-center border-heroTheme-300 text-white ">
+      <h1 className="mt-40 flex w-full items-center justify-center text-3xl uppercase">
         It's dangerous to go into the world of web development alone.
       </h1>
-      <h1 className="w-full uppercase text-3xl flex items-center justify-center">
+      <h1 className="flex w-full items-center justify-center text-3xl uppercase">
         Take this!
       </h1>
-      <h2 className="w-full text-3xl flex items-center justify-center mt-32">
+      <h2 className="mt-12 flex w-full items-center justify-center text-3xl">
         Name: Aaron Allen
       </h2>
-      <h2 className="w-full text-3xl flex items-center justify-center mt-4">
+      <h2 className="mt-4 flex w-full items-center justify-center text-3xl">
         Email: aallen0001@gmail.com
       </h2>
-      <h3 className="w-full uppercase text-3xl flex items-center justify-center mt-12">
+      <h3 className="mt-10 flex w-full items-center justify-center text-3xl uppercase">
         Thanks for Visiting!
       </h3>
       <Link to="/">
-        <button className="bg-heroTheme-400 text-heroTheme-300 inline-flex px-7 py-3.5 text-portfolioBTN uppercase font-serounded-lg text-center text-shadow font-buttonText border-t-[.3px] border-l-[.3px] border-r-[3px] border-b-[3px] shadow-[4px_4px_4.8px_0px_rgba(0,0,0,0.2)] border-heroTheme-300 rounded-md mt-12">
-          Play Again?
+        <button className="font-serounded-lg text-shadow mt-6 rounded-md px-7 py-3.5 text-center font-buttonText text-portfolioNG uppercase text-white hover:animate-pulse hover:border-b-[3px] hover:border-l-[.3px] hover:border-r-[3px] hover:border-t-[.3px] hover:border-heroTheme-300 hover:bg-emerald-500 hover:text-black hover:shadow-[4px_4px_4.8px_0px_rgba(0,0,0,0.2)]">
+          Back to Main Menu
         </button>
       </Link>
     </div>
