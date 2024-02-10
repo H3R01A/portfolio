426adbdc74ebc140960f2c51de912cbc9275898d
H3R01A <aallen0001@gmail.com>
2024-02-10 14:02:07 -0800

    Update App component with Home button styling and about page link

--- client/App.tsx
+++ client/App.tsx
@@ -3,6 +3,7 @@ import ContactPage from './components/ContactPage';
 import WelcomePage from './components/WelcomePage';
 import Project from './components/Project';
 import { Routes, Route, Link } from 'react-router-dom';
+import AboutPage from './components/AboutPage';
 
 export default function App() {
   const [projectNum, setProjectNum] = useState(1);
@@ -14,10 +15,12 @@ export default function App() {
   };
   return (
     <>
-      <div className="p-10 uppercase text-heroTheme-500">
+      <div className="p-10 uppercase text-white ">
         {/* <p className='text-blue-100'>Home</p> */}
-        <Link to="/" className="font-bold">
-          Home
+        <Link to="/" className="font-bold ">
+          <button className="font-serounded-lg text-shadow rounded-md px-7 py-3.5 text-center font-buttonText text-portfolioNG uppercase text-white hover:animate-pulse hover:border-b-[3px] hover:border-l-[.3px] hover:border-r-[3px] hover:border-t-[.3px] hover:border-heroTheme-300 hover:bg-emerald-500 hover:text-black hover:shadow-[4px_4px_4.8px_0px_rgba(0,0,0,0.2)]">
+            Home
+          </button>
         </Link>
       </div>
       <Routes>
@@ -26,6 +29,7 @@ export default function App() {
           element={<WelcomePage setProjectNum={setProjectNum} />}
         ></Route>
         <Route path="/contact" element={<ContactPage />} />
+        <Route path="/about" element={<AboutPage />} />
         <Route
           path="/project"
           element={
