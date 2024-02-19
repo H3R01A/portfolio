4e2e1063e493f6a5fbff74d41fcc22b4d29224d0
H3R01A <aallen0001@gmail.com>
2024-02-19 10:59:56 -0800

    update App component with styling to make Home button not shift rest of elements on page and to remove unnecessary styling in Link div parent

--- client/App.tsx
+++ client/App.tsx
@@ -15,10 +15,10 @@ export default function App() {
   };
   return (
     <>
-      <div className="p-10 uppercase text-white ">
+      <div className="p-10 ">
         {/* <p className='text-blue-100'>Home</p> */}
         <Link to="/" className="font-bold ">
-          <button className="font-serounded-lg text-shadow rounded-md px-7 py-3.5 text-center font-buttonText text-portfolioNG uppercase text-white hover:animate-pulse hover:border-b-[3px] hover:border-l-[.3px] hover:border-r-[3px] hover:border-t-[.3px] hover:border-heroTheme-300 hover:bg-emerald-500 hover:text-black hover:shadow-[4px_4px_4.8px_0px_rgba(0,0,0,0.2)]">
+          <button className="font-serounded-lg text-shadow rounded-md border-2 border-transparent px-7 py-3.5 text-center font-buttonText text-portfolioNG uppercase text-white hover:animate-pulse hover:border-b-[3px] hover:border-l-[.3px] hover:border-r-[3px] hover:border-t-[.3px] hover:border-heroTheme-300 hover:bg-emerald-500 hover:text-black hover:shadow-[4px_4px_4.8px_0px_rgba(0,0,0,0.2)]">
             Home
           </button>
         </Link>
