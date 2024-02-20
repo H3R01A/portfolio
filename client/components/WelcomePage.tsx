f87a39c6045349aee68633d9069c865016ad4244
H3R01A <aallen0001@gmail.com>
2024-02-19 19:29:11 -0800

    update WelcomePage to fix buttons and update styling

--- client/components/WelcomePage.tsx
+++ client/components/WelcomePage.tsx
@@ -17,19 +17,19 @@ export default function WelcomePage({ setProjectNum }: WelcomeProps) {
 
   return (
     <>
-      <section className="m-72 flex flex-col text-center">
+      <section className="m-72 flex flex-col gap-y-6 text-center">
         <Link to="/project">
-          <button className="font-serounded-lg text-shadow rounded-md px-7 py-3.5 text-center font-buttonText text-portfolioNG uppercase text-white hover:animate-pulse hover:border-b-[3px] hover:border-l-[.3px] hover:border-r-[3px] hover:border-t-[.3px] hover:border-heroTheme-300 hover:bg-emerald-500 hover:text-black hover:shadow-[4px_4px_4.8px_0px_rgba(0,0,0,0.2)]">
+          <button className="font-serounded-lg text-shadow rounded-md border-2 border-transparent px-7 py-3.5 text-center font-buttonText text-portfolioNG uppercase text-white hover:animate-pulse hover:border-b-[3px] hover:border-l-[.3px] hover:border-r-[3px] hover:border-t-[.3px] hover:border-heroTheme-300 hover:bg-emerald-500 hover:text-black hover:shadow-[4px_4px_4.8px_0px_rgba(0,0,0,0.2)]">
             New Game
           </button>
         </Link>
         <Link to="/about">
-          <button className="font-serounded-lg text-shadow rounded-md px-7 py-3.5 text-center font-buttonText text-portfolioNG uppercase text-white hover:animate-pulse hover:border-b-[3px] hover:border-l-[.3px] hover:border-r-[3px] hover:border-t-[.3px] hover:border-heroTheme-300 hover:bg-emerald-500 hover:text-black  hover:shadow-[4px_4px_4.8px_0px_rgba(0,0,0,0.2)]">
+          <button className="font-serounded-lg text-shadow rounded-md border-2 border-transparent px-7 py-3.5 text-center font-buttonText text-portfolioNG uppercase text-white hover:animate-pulse hover:border-b-[3px] hover:border-l-[.3px] hover:border-r-[3px] hover:border-t-[.3px] hover:border-heroTheme-300 hover:bg-emerald-500 hover:text-black  hover:shadow-[4px_4px_4.8px_0px_rgba(0,0,0,0.2)]">
             About
           </button>
         </Link>
         <Link to="/contact">
-          <button className="font-serounded-lg text-shadow rounded-md px-7 py-3.5 text-center font-buttonText text-portfolioNG uppercase text-white hover:animate-pulse hover:border-b-[3px] hover:border-l-[.3px] hover:border-r-[3px] hover:border-t-[.3px] hover:border-heroTheme-300 hover:bg-emerald-500 hover:text-black  hover:shadow-[4px_4px_4.8px_0px_rgba(0,0,0,0.2)]">
+          <button className="font-serounded-lg text-shadow rounded-md border-2 border-transparent px-7 py-3.5 text-center font-buttonText text-portfolioNG uppercase text-white hover:animate-pulse hover:border-b-[3px] hover:border-l-[.3px] hover:border-r-[3px] hover:border-t-[.3px] hover:border-heroTheme-300 hover:bg-emerald-500 hover:text-black  hover:shadow-[4px_4px_4.8px_0px_rgba(0,0,0,0.2)]">
             Contact
           </button>
         </Link>
