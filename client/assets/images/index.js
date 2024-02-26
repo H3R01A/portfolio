8d7fedda6450d289260c7f0300c6cb1df830984b
H3R01A <aallen0001@gmail.com>
2024-02-25 21:04:28 -0800

    add images folder for better organization. Also add index.js to export images

---
+++ client/assets/images/index.js
@@ -0,0 +1,17 @@
+import dailyquest1 from './daily-quest-1.png';
+import dailyquest2 from './daily-quest-2.png';
+import codenamehermes from './codename-hermes.png';
+import subslify1 from './subslify-1.png';
+import subslify2 from './subslify-2.png';
+import subslify3 from './subslify-3.png';
+import subslify4 from './subslify-4.png';
+
+export default {
+    dailyquest1,
+    dailyquest2,
+    codenamehermes,
+    subslify1,
+    subslify2,
+    subslify3,
+    subslify4,
+  };
